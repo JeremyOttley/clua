e88a839fe9be1d3bf1ae94f8ef6b66f7b6979198
jeremyottley <jeremy.ottley@gmail.com>
2018-12-27 11:10:34 -0500

    new data

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <lua.h> // Lua basic definitions
+#include <lualib.h> // Lua standard libraries
+#include <luaxlib.h> // Lua auxiliary functions
+#include "libcrap_lua.h" // our Lua exported functions
 
+int main(int argc, char *argv[]){
+  lua_State* lua = luaL_newstate();
+  luaL_openlibs(lua);
+  glue(lua)' //WTF?
+  luaL_dofile(lua, "./app.lua")
+  lua_close(lua);
+}
